69baf643516c3bc1d78df3b5eaccb4a5724d6f70
Marius Kavoliunas <marius.kavoliunas@telesoftas.com>
2020-06-23 17:47:40 +0300

    test implicit api
    on-behalf-of: @TeleSoftas <marius.kavoliunas@telesoftas.com>

---
+++ src/test/scala/com/telesoftas/ijplugin/gitconfigcommittemplate/ImplicitApisTest.scala
@@ -0,0 +1,47 @@
+package com.telesoftas.ijplugin.gitconfigcommittemplate
+
+import java.io._
+
+import com.intellij.openapi.project._
+import com.intellij.openapi.vfs._
+import org.mockito.ArgumentMatchersSugar
+import org.mockito.MockitoSugar
+import org.scalatest.flatspec._
+import org.scalatest.matchers.should._
+
+class ImplicitApisTest extends AnyFlatSpec with Matchers with MockitoSugar with ArgumentMatchersSugar {
+
+  "ImplicitsFileApi" should "return parent" in {
+    new File(".").getAbsoluteFile.parent shouldBe defined
+    new File(".").parent shouldBe empty
+  }
+
+  it should "read file" in {
+    new File("src/test/resources/nonEmptyFile").read shouldBe Some("contents")
+    new File("src/test/resources/nonExisting").read shouldBe empty
+  }
+
+  it should "list contents" in {
+    new File("src/test/resources").contents shouldNot be(List.empty)
+    new File("src/test/voidness").contents shouldBe empty
+  }
+
+  it should "look parent up until it matches condition" in {
+    new File("src/test/resources/nonEmptyFile").lookUp(_.getName == "test") shouldBe defined
+    new File("src/test/resources/nonEmptyFile").lookUp(_.getName.contains("!@#$!@#$!%#$%")) shouldBe empty
+  }
+
+  "ImplicitProjectApi" should "find repoRoot" in {
+    val repoRoot            = mock[VirtualFile]
+    val mockLocalFileSystem = mock[LocalFileSystem]
+    when(mockLocalFileSystem.findFileByIoFile(any[File])).thenReturn(repoRoot)
+    val mockProject         = mock[Project]
+
+    when(mockProject.getProjectFilePath).thenReturn("src/test/resources/")
+    mockProject.repoRoot(mockLocalFileSystem) shouldBe Some(repoRoot)
+
+    when(mockProject.getProjectFilePath).thenReturn("/")
+    mockProject.repoRoot(mockLocalFileSystem) shouldBe empty
+  }
+
+}
