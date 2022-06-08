ad806c6b9fb92e5389c3b1193aea00bec107dcc3
Marius Kavoliūnas <marius.kavoliunas@telesoftas.com>
2022-06-08 23:33:42 +0300

    gh-13: implement empty message notification as dialog

--- src/main/scala/com/telesoftas/ijplugin/gitconfigcommittemplate/CustomCheckinHandler.scala
+++ src/main/scala/com/telesoftas/ijplugin/gitconfigcommittemplate/CustomCheckinHandler.scala
@@ -1,60 +1,51 @@
 package com.telesoftas.ijplugin.gitconfigcommittemplate
 
-import com.intellij.notification.NotificationType._
-import com.intellij.notification._
 import com.intellij.openapi._
 import com.intellij.openapi.project._
+import com.intellij.openapi.ui.Messages
 import com.intellij.openapi.vcs.checkin.CheckinHandler._
 import com.intellij.openapi.vcs.checkin._
 import com.intellij.openapi.vcs.ui._
-import com.telesoftas.ijplugin.gitconfigcommittemplate.wrapper.CommitMessage
-import com.telesoftas.ijplugin.gitconfigcommittemplate.wrapper.CommitMessage._
+import com.telesoftas.ijplugin.gitconfigcommittemplate.wrapper.CommitMessageField
+import com.telesoftas.ijplugin.gitconfigcommittemplate.wrapper.CommitMessageField._
+import git4idea.i18n.GitBundle
 
 class CustomCheckinHandler extends CheckinHandler {
 
-  private var project: Option[Project]                     = None
-  private var commitMessage: Option[wrapper.CommitMessage] = None
+  private var project: Option[Project]                       = None
+  private var commitMessageField: Option[CommitMessageField] = None
 
   override def getAfterCheckinConfigurationPanel(disposable: Disposable): RefreshableOnComponent = {
     project = disposable.doIf(classOf[CommitMessageHandler])(c => Option(c.getWorkflow.getProject))
-    commitMessage = wrapper.CommitMessage(disposable)
+    commitMessageField = CommitMessageField(disposable)
     super.getAfterCheckinConfigurationPanel(disposable)
   }
 
-  override def beforeCheckin(): CheckinHandler.ReturnResult =
-    if (commitMessage.isDefined) {
-      val result = for {
-        message      <- commitMessage
-        finalMessage <- filteredMessage(message)
-        _             = message.set(finalMessage)
-      } yield ReturnResult.COMMIT
-
-      result.toRight(CommitNotification.show(project, _.MessageCanNotStartWithComment)).getOrElse(ReturnResult.CANCEL)
-    } else super.beforeCheckin()
-
-  private def filteredMessage(commitMessage: CommitMessage): Option[String] = {
-    val trimmedInLines = commitMessage.get.trim.split("\n")
-    val usefulLines    = trimmedInLines.filterNot(line => line.trim.isEmpty || line.startsWith("#"))
-    if (usefulLines.isEmpty) None
-    else Some(trimmedInLines.filterNot(_.trim.startsWith("#")).mkString("\n"))
+  override def beforeCheckin(): CheckinHandler.ReturnResult = {
+    commitMessageField.fold(super.beforeCheckin()) { messageField =>
+      filteredMessage(messageField.getText) match {
+        case Some(message) =>
+          messageField.setText(message)
+          ReturnResult.COMMIT
+        case None          =>
+          project.foreach(
+            Messages.showMessageDialog(
+              _,
+              "Specify message in uncommented line",
+              GitBundle.message("git.commit.message.empty.title"),
+              Messages.getErrorIcon,
+            ),
+          )
+          ReturnResult.CANCEL
+      }
+    }
   }
 
-}
-
-object CommitNotification {
-
-  private val LocalGroup = new NotificationGroup(
-    getClass.getCanonicalName,
-    NotificationDisplayType.BALLOON,
-    false,
-  )
-
-  def show(project: Option[Project], f: NotificationInGroup.type => Notification): Unit =
-    project.foreach(p => Notifications.Bus.notify(f(NotificationInGroup), p))
-
-  object NotificationInGroup {
-    def MessageCanNotStartWithComment: Notification =
-      LocalGroup.createNotification("Commit message can't start with a comment", WARNING)
+  private def filteredMessage(commitMessage: String): Option[String] = {
+    val trimmedInLines = commitMessage.trim.split("\n").map(_.trim)
+    val finalLines     = trimmedInLines.filterNot(line => line.startsWith("#"))
+    if (finalLines.exists(_.nonEmpty)) Some(finalLines.mkString("\n"))
+    else None
   }
 
 }
